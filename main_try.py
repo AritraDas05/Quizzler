631d01068c0d68cfb9893c85a90531d9a5871733
AritraDas05 <aritra2005das@gmail.com>
2025-11-05 08:13:34 +0000

    The problem bank abstract is created

--- main_try.py
+++ main_try.py
@@ -1,8 +1,5 @@
-# main.py
 from problems_bank_abstract import ProblemBank
 from problems_bank_opentb import OpenTBProblemBank
-# In the future, you could also have:
-# from problems_bank_local import LocalProblemBank
 
 def load_and_show_questions(bank: ProblemBank):
     bank.load_questions()
@@ -12,9 +9,6 @@ def load_and_show_questions(bank: ProblemBank):
 if __name__ == "__main__":
     parameters = {"amount": 5, "type": "multiple", "category": 19}
 
-    # You can swap this line and everything else still works:
     problem_bank = OpenTBProblemBank(parameters)
-    # problem_bank = LocalProblemBank("data/questions.json")  # also valid later
 
-    # Pass it as a ProblemBank (abstraction, not concrete type)
     load_and_show_questions(problem_bank)
